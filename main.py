bfecff1e5b2e692700fc42a97a22899fb9956c59
antosha <antosha@Ubuntu.com.AntoshaUnique>
2023-01-08 21:58:00 +0500

    update API scrypt

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-from fastapi import FastAPI
+import os
+
+import shutil
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+import requests
 # from путь к нашей программе
 
 app = FastAPI()
-# здесь достается наша прога
+# app.mount("/static", StaticFiles(directory = "public"))
 
 
-# get-запрос на файл, который должен проверятья
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello"}
+async def hello():
+    return {"message": "Hello world"}
 
-# post-запрос с выводом программы
-@app.post("/predict/")
-def predict():
-    return {"message": "Hello"}    
+
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile = File(...)):
+    try:
+        # content = file.file.read()  
+        with open(file.filename, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+    except Exception:
+        return {"message":"ERROR uploading file"}
+    finally:
+        file.file.close()
+    with open(file.filename, "r") as f:
+        for line in f:
+            a = str(line)
+            break
+        f.close()
+    os.remove(file.filename)
+    return {"text": a}
+
+
+
+
+# @app.post("/uploadfile")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     try:
+#         with open(file.filename, "r") as f:
+#             for line in f:
+#                 a = str(line)
+#     except Exception:
+#         return {"message":"ERROR1"}
+#     finally:
+#         file.file.close()    
+#     return {"text": a}
+
+
+
+
+
