5800ec678bf2fb46251aa72bcdca6f3f1942ff6c
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-01-09 23:11:36 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,6 +3,23 @@ from fastapi.testclient import TestClient
 app = FastApi()
 client = TestClient(app)
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+  
 def test_read_main():
   response = client.get('/')
   assert response.status_code == 200
+  assert response.json() == {"message": "Hello World"}
+
+# def test_with_pass():
+#   response = client.post(''),
+#       file=''
+#   )
+#   file_data = 
+
+#   assert = assert response.status_code == 200
+#   assert file_data == ''
+
+# def test_without_pass():
+  
