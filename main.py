4b3db4e1f449ec70a6b2bef54d2e80358a7b00d9
antosha <antosha@Ubuntu.com.AntoshaUnique>
2023-01-09 21:48:20 +0500

    updated API script

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-import requests
+import uvicorn
 
 from project_program import model1, model2
 # from путь к нашей программе
@@ -15,35 +15,36 @@ app = FastAPI()
 
 
 
-while True:
-    @app.get("/")
-    async def hello():
-        return {"message": "Hello world"}
+@app.get("/")
+async def hello():
+    return {"message": "Hello world"}
 
 
-    @app.post("/uploadfile")
-    async def create_upload_file(file: UploadFile = File(...)):
-        try:
-            # content = file.file.read()  
-            with open(file.filename, "wb") as f:
-                shutil.copyfileobj(file.file, f)
-        except Exception:
-            return {"message":"ERROR uploading file"}
-        finally:
-            file.file.close()
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile = File(...)):
+    try:
+        # content = file.file.read()  
+        with open(file.filename, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+    except Exception:
+        return {"message":"ERROR uploading file"}
+    finally:
+        file.file.close()
 
-            
-        with open(file.filename, 'r') as f:
-            # Работа первой модели
-            passwords_mas = model1(file.filename)
-            # Работа второй модели
-            res_preds = model2(file.filename)
-            #Файл json с результатом
-            snippets_with_pass = res_preds[res_preds['Target'] == 1]['Snippet'].tolist()
+        
+    with open(file.filename, 'r') as f:
+        # Работа первой модели
+        passwords_mas = model1(file.filename)
+        # Работа второй модели
+        res_preds = model2(file.filename)
+        #Файл json с результатом
+        snippets_with_pass = res_preds[res_preds['Target'] == 1]['Snippet'].tolist()
 
-        os.remove(file.filename)
-        return {"snippets": snippets_with_pass}
-else: time.sleep(1)
+    os.remove(file.filename)
+    return {"snippets": snippets_with_pass}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload = True)
 
 
 
