6c7d8d3164a47a0be5628c05e7bc18fadebe86fe
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-01-13 01:28:01 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,18 +4,14 @@ from main import app
 
 client = TestClient(app)
   
-def test_read_main():
-  response = client.get('/')
-  assert response.status_code == 200
-  assert response.json() == {"message": "Hello World"}
+# def test_read_main():
+#   response = client.get('/')
+#   assert response.status_code == 200
+#   assert response.json() == {"message": "Hello World"}
 
 # def test_with_pass():
-#   response = client.post(''),
-#       file=''
-#   )
-#   file_data = 
-
-#   assert = assert response.status_code == 200
+#   response = 
+  
 #   assert file_data == ''
 
 # def test_without_pass():
