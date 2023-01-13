fa8f67ee2cac47077c8e5d2307fc5b64ca774f65
M1nestreL <115129624+M1nestreL@users.noreply.github.com>
2023-01-13 20:43:01 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,10 +4,10 @@ from main import app
 
 client = TestClient(app)
   
-# def test_read_main():
-#   response = client.get('/')
-#   assert response.status_code == 200
-#   assert response.json() == {"message": "Hello World"}
+def test_read_main():
+  response = client.get('/')
+  assert response.status_code == 200
+  assert response.json() == {"message": "Hello World"}
 
 # def test_with_pass():
 #   response = 
