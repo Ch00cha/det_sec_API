232ef53e068f6431c35ffc523580b24e783407e0
antosha <antosha@Ubuntu.myguest.virtualbox.org>
2023-01-14 16:26:22 +0500

    added post with text request

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,20 +1,29 @@
 import os
 import time
 import shutil
-from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import FileResponse
+import base64
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-# from project_program import find_secrets
-# from путь к нашей программе
+from project_program import find_secrets
+
+
+def find_secrets_in_text(file_name, content):
+    content = base64.b64decode(content).decode()
+    file = open(str(file_name), 'w')
+    file.write(content)
+    file.close()
+    predictions = find_secrets(str(file_name))
+    os.remove(str(file_name))
+    return predictions
 
 class Item(BaseModel):
-    text: str
+    path: list
+    content: list
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory = "public"))
+
 
 
 @app.get("/")
@@ -22,21 +31,29 @@ async def hello():
     return {"message": "Hello world"}
 
 
+@app.post("/predict")
+async def predict(item: Item):
+    result = {}
+    read_json = Item.parse_raw(item)
+    for i in range(len(read_json.path)):
+        file_path = read_json.path[i]
+        predictions = find_secrets_in_text(file_path, read_json.content[i])
+        result.update({file_path : predictions})
+    return result
+    
+
+
+
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile = File(...)):
     try:
-        # content = file.file.read()  
         with open(file.filename, "wb") as f:
             shutil.copyfileobj(file.file, f)
     except Exception:
         return {"message":"ERROR uploading file"}
     finally:
-        file.file.close()
-
-        
-    with open(file.filename, 'r') as f:
-        # Работа второй модели
-        res_preds = len(file.filename)
+        file.file.close()  
+    res_preds = find_secrets(file.filename)
     f_name = file.filename
     os.remove(file.filename)
     return {f_name : res_preds} 
@@ -47,18 +64,6 @@ if __name__ == "__main__":
 
 
 
-
-# @app.post("/uploadfile")
-# async def create_upload_file(file: UploadFile = File(...)):
-#     try:
-#         with open(file.filename, "r") as f:
-#             for line in f:
-#                 a = str(line)
-#     except Exception:
-#         return {"message":"ERROR1"}
-#     finally:
-#         file.file.close()    
-#     return {"text": a}
 
 
 
