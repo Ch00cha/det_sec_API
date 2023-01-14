4b393d0ebe9a096ea4d18ea3c34536f76194da97
antosha <antosha@Ubuntu.myguest.virtualbox.org>
2023-01-15 04:40:46 +0500

    updated api script

--- fastapi_app.py
+++ fastapi_app.py
@@ -4,39 +4,47 @@ import shutil
 import base64
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+from typing import List
 import uvicorn
-
 from project_program import find_secrets
 
 
-def find_secrets_in_text(file_name, content):
+def find_secrets_in_text(content):
     content = base64.b64decode(content).decode()
-    file = open(str(file_name), 'w')
+    file = open("file.txt", 'w')
     file.write(content)
     file.close()
-    predictions = find_secrets(str(file_name))
-    os.remove(str(file_name))
-    return predictions
+    try:
+        predictions = find_secrets("file.txt")
+    except:
+        predictions = {"Error": "Ошибка сканирования файла"}
+        os.remove("file.txt")
+        return predictions
+    else:
+        os.remove("file.txt")
+        return predictions
+
+
+class Data(BaseModel):
+    content: str
+    name: str
 
 class Item(BaseModel):
-    path: list
-    content: list
+    accept: List[Data]
 
 app = FastAPI()
-
-
 
 @app.get("/")
 async def hello():
     return {"message": "Hello world"}
 
 @app.post("/predict")
-async def predict(item: Item):
+def predict(item: Item):
     result = {}
-    read_json = Item.parse_raw(item)
-    for i in range(len(read_json.path)):
-        file_path = read_json.path[i]
-        predictions = find_secrets_in_text(file_path, read_json.content[i])
+    read_json = Item.parse_obj(item)
+    for i in range(len(read_json.accept)):
+        file_path = read_json.accept[i].name
+        predictions = find_secrets_in_text(read_json.accept[i].content)
         result.update({file_path : predictions})
     return result
     
@@ -56,7 +64,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("fastapi_app:app", host="0.0.0.0", reload = True)
+    uvicorn.run("fastapi_app:app", host="127.0.0.1", reload = True)
 
 
 
