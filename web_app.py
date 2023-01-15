97eb91a14b8a6f1708c13186db163b52b14d5ca0
Слава <this-is-yashka@yandex.ru>
2023-01-15 15:04:35 +0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import app
+from api_requests import request_to_det_sec_API
 
 st.title('Детекция секретов в коде')
 st.markdown('Проверьте свой репозиторий или файл из репозитория на наличие  **секретов**.')
@@ -9,26 +9,9 @@ url = st.text_input('dd', '', placeholder='URL', label_visibility="hidden")
 if st.button('Найти все секреты'):
     with st.spinner('Ждём жоского грязного белья...'):
         try:
-            output = app.test1() #url внутри
+            response = request_to_det_sec_API(url)
             st.markdown('Мы тут несколько **секретов** у Вас нашли:')
-            st.write(output)
-            st.success('Дело сделано', icon='✅')
-        except Exception:
-            st.error('Неверный URL а репозиторий', icon="🚨")
-# import streamlit as st
-# #import app
-# import numpy as np
-# st.title('Детекция секретов в коде')
-# st.markdown('Проверьте свой репозиторий или файл из репозитория на наличие  **секретов**.')
-# st.markdown('Вставьте сюда :point_down: _ссылку_ на репозиторий или файл')
-# url = st.text_input('dd', '', placeholder='URL', label_visibility="hidden")
-
-# if st.button('Найти все секреты'):
-#     with st.spinner('Ждём жоского грязного белья...'):
-#         try:
-#             output = app.test1() #url внутри
-#             st.markdown('Мы тут несколько **секретов** у Вас нашли:')
-#             st.write(output)
-#             st.success('Дело сделано', icon='✅')
-#         except Exception:
-#             st.error('Неверный URL а репозиторий', icon="🚨")
+            st.write(response)
+            st.success('Дело сделано', icon="✅")
+        except KeyErrror:
+            st.error('Неверный URL или репозиторий приватный', icon="🚨")
