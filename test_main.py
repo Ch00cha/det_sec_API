02469445976597cf4312249ac0e614c32bf24914
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-01-15 12:35:06 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,10 +6,10 @@ from project_program import find_secrets
 client = TestClient(app)
 
 
-# def test_read_main():
-#     response = client.get('/')
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Hello world"}
+def test_read_main():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world"}
 
 
 def test_model():
