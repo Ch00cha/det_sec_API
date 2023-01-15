b0f84e17cddbd1cebaf7f5ffdad02fba53d232f4
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-01-15 19:19:36 +0500

    Update api_requests.py

--- api_requests.py
+++ api_requests.py
@@ -20,19 +20,25 @@ def github_scan_rep(repository_path):
             file_path += i + '/'
     url = f'https://api.github.com/repos/{get_username}/{get_repository_name}/contents/{file_path}'
     r = requests.get(url, headers=headers)
-    data = r.json()
-    if file_path != '' and data['type']=='file':
-        name = data['name']
-        content = data['content']
-        check.append({'name':name, 'content': content})
+    if str(r) == '<Response [403]>':
+        return 'Превышено количество запросов'
+    elif str(r) == '<Response [404]>':
+        return 'Неверный url, либо репозиторий является приватным'
     else:
-        for each_file in data:
-            if each_file['type']=='file':
-                name = each_file['name']
-                each_file = requests.get(str(each_file['url']), headers = headers).json()
-                content = each_file['content']
-                check.append({'name':name, 'content': content})
-    return check
+        data = r.json()
+        if file_path != '' and data['type']=='file':
+            name = data['name']
+            content = data['content']
+            check.append({'name':name, 'content': content})
+        else:
+            for each_file in data:
+                if each_file['type']=='file':
+                    name = each_file['name']
+                    each_file = requests.get(str(each_file['url']), headers = headers).json()
+                    content = each_file['content']
+                    check.append({'name':name, 'content': content})
+        return check
+
 
 def request_to_det_sec_API(url):
     data = github_scan_rep(url)
@@ -40,4 +46,3 @@ def request_to_det_sec_API(url):
     ip = read_ip()
     r = requests.post(f'http://{ip}:8000/predict',  data = json.dumps(data)).json()
     return r
-
