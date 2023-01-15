64a9c880ef00d58858939c124999518b436e352f
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-01-15 16:49:34 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from fastapi_app import app
-from fastapi_app import find_secrets
+from fastapi_app import app, find_secrets
+from api_requests import github_scan_rep
 
 client = TestClient(app)
 
@@ -17,22 +17,21 @@ def test_model_with_pass():
     assert prediction == {3: 'String password = ""Stargate1"";'}
     
     
-def test_model_with_pass():
+def test_model_without_pass():
     prediction = find_secrets('snip_without_pass.txt')
     assert prediction == 'Пароли не найдены'
 
     
-def test_with_pass():
-    response = client.post('/uploadfile', input = 'snipet_with_pass.txt')
-    assert response.status_code == 200
-    assert response == {3: 'String password = ""Stargate1"";'}
+# def test_with_pass():
+#     response = client.post('/uploadfile', input = 'snipet_with_pass.txt')
+#     assert response.status_code == 200
+#     assert response == {3: 'String password = ""Stargate1"";'}
 
 
 def test_repo():
-    response = client.post('/predict', url = 'https://github.com/Ch00cha/det_sec_API')
+    content = github_scan_rep('https://github.com/Ch00cha/det_sec_API')
+    response = client.post('/predict', json=content)
     assert response.status_code == 200
     assert response == {'README.md': 'Пароли не найдены', 'api_requests.py': 'Пароли не найдены', 'fastapi_app.py': 'Пароли не найдены', 'model_app.py': 'Пароли не найдены', 'requirements.txt': 'Пароли не найдены', 'test_file.txt': {'3': 'String password = ""Stargate1"";'}, 'test_main.py': 'Пароли не найдены', 'web_app.py': 'Пароли не найдены'}
 
-
-# def test_without_pass():
   
